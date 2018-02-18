a810f39029d018058e303f980b81bf98b6f05566
Nicholas Erskine <nicholas.erskine95@gmail.com>
2018-02-18 13:44:47 +1100

    Add error handling code - scraper now waits and tries again instead of giving up when download fails

--- R/load_statsguru_results.R
+++ R/load_statsguru_results.R
@@ -11,7 +11,12 @@ load_statsguru_results <- function(url) {
     
     while (!finished) {
         page_url <- paste0(url, ";page=", page)
-        html_raw <- xml2::read_html(page_url)
+        success <- class(try(html_raw <- xml2::read_html(page_url)))!='try-error'
+	if (!success){
+		print('Failure to dowload webpage. Sleeping ten seconds before trying again')
+		Sys.sleep(10)
+		next()
+	}
         new_data <- rvest::html_table(html_raw, fill = TRUE)[[3]]
         new_data <- new_data[, names(new_data) != ""]
         if (new_data[[1]][[1]] == "No records available to match this query") {
